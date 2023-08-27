83502412fffdda00c8000605d49af952a0c4915b
Cesar Aguado <cesaraguado123@gmail.com>
2023-08-27 18:26:51 -0500

    second class typescript

--- clases/homeworks/ejercicio 3.ts
+++ clases/homeworks/ejercicio 3.ts
@@ -7,10 +7,11 @@ let dynamicList: any[] = ["Franco", 26, true];
 
 // Tuple
 let person: [string, number] = ["Franco", 26];
-//let extendIncorrectPerson: [string, number] = ["Franco", 26, true]; // Error
+let extendIncorrectPerson: [string, number] = ["Franco", 26]; // Error
 
 //crea una tupla, donde en la primera posicion haya un booleano y en la segunda un string
-
+let exampleTupla: [boolean, string] = [true, 'letsGo']
 //crea un arreglo de strings
+let arrayStrings: string[] = ['if you can', 'ready for all', 'letsDo']
 
 export {}
